b5f9589fb9f18a6ed9c7cb1ee84ef137bfa9cf63
Jason <jason.bautista@sqor.com>
2013-11-18 17:02:28 -0800

    Running local or global tests now

--- basicExample.py
+++ basicExample.py
@@ -1,30 +1,51 @@
-import time
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
 from selenium.webdriver.support import expected_conditions as EC # available since 2.26.0
 
 
-driver= webdriver.Chrome()
+# driver= webdriver.Chrome()
+
+
+desired_capabilities = webdriver.DesiredCapabilities.IPHONE
+desired_capabilities['version'] = '5.0'
+desired_capabilities['platform'] = 'MAC'
+desired_capabilities['name'] = 'Testing Selenium 2 in Python at Sauce'
+
+driver = webdriver.Remote(
+    desired_capabilities=desired_capabilities,
+    #command_executor="http://username-string:access-key-string@ondemand.saucelabs.com:80/wd/hub"
+    command_executor="http://sqordev:d9c8fd8c-296b-4f71-997b-6a33bc80c483@ondemand.saucelabs.com:80/wd/hub"
+)
+
+driver.implicitly_wait(30)
+
+driver= webdriver.remote(
+        "ondemand.saucelabs.com"
+        , 80
+        , "sqordev"
+        , "d9c8fd8c-296b-4f71-997b-6a33bc80c483"
+);
+
 driver.get('http://sqorweb.local')
 
 try:
     link = driver.find_element_by_css_selector(".dropdown");
     link.click()
 
-    time.sleep(1)
+    #time.sleep(1)
     links = link.find_elements_by_tag_name("a")
     links[2].click()
 
-    time.sleep(2)
+    #time.sleep(2)
     driver.get('http://sqorweb.local/team/551/nba/los-angeles-lakers')
 
-    time.sleep(2)
+    #time.sleep(2)
     links = driver.find_elements_by_tag_name("a")
-    time.sleep(2)
+    #time.sleep(2)
     links[15].click()
     driver.back()
-    time.sleep(4)
+    #time.sleep(4)
 
 
     '''
@@ -32,23 +53,23 @@ try:
     teamLink = driver.find_elements_by_css_selector(".team-list-entry")
     teamLink[0].click()
     '''
-    time.sleep(2)
+    #time.sleep(2)
 
 
     # Loop
     link = driver.find_element_by_css_selector(".dropdown");
     link.click()
-    time.sleep(2)
+    #time.sleep(2)
     links = link.find_elements_by_tag_name("a")
     links[3].click()
-    time.sleep(2)
+    #time.sleep(2)
 
 
 
     # Loop
     link = driver.find_element_by_css_selector(".dropdown");
     link.click()
-    time.sleep(1)
+    #time.sleep(1)
     links = link.find_elements_by_tag_name("a")
     links[1].click()
 
@@ -58,17 +79,17 @@ try:
     # Loop
     link = driver.find_element_by_css_selector(".dropdown");
     link.click()
-    time.sleep(2)
+    #time.sleep(2)
     links = link.find_elements_by_tag_name("a")
     links[4].click()
-    time.sleep(2)
+    #time.sleep(2)
     driver.back()
-    time.sleep(2)
+    #time.sleep(2)
 
 
 except (RuntimeError, ValueError, NameError):
     driver.quit()
 finally:
-    time.sleep(5)
+    #time.sleep(5)
     driver.quit()
 
---
+++ seleniumExample.py
@@ -0,0 +1,58 @@
+import unittest
+import os
+from selenium import webdriver
+
+
+class Selenium2OnSauce(unittest.TestCase):
+
+    def setUp(self):
+        desired_capabilities = webdriver.DesiredCapabilities.IPHONE
+        desired_capabilities['version'] = '5.0'
+        desired_capabilities['platform'] = 'MAC'
+        desired_capabilities['name'] = 'Testing Selenium 2 in Python at Sauce'
+
+        #
+        shouldRunLocal =  os.environ.get("randomrandomrandomwfdsf342342", "local")
+
+
+        if (shouldRunLocal == "local"):
+            print "Running locally"
+            self.driver = webdriver.Chrome()
+        else:
+            print "Running remotely"
+            self.driver = webdriver.Remote(
+                desired_capabilities=desired_capabilities,
+                command_executor="http://sqordev:d9c8fd8c-296b-4f71-997b-6a33bc80c483@ondemand.saucelabs.com:80/wd/hub"
+            )
+        self.driver.implicitly_wait(30)
+
+    def test_sauce(self):
+        self.driver.get('http://saucelabs.com/test/guinea-pig')
+        self.assertTrue("I am a page title - Sauce Labs" in self.driver.title)
+        comments = self.driver.find_element_by_id('comments')
+        comments.send_keys('Hello! I am some example comments.'
+                           ' I should be in the page after submitting the form')
+        self.driver.find_element_by_id('submit').click()
+
+        commented = self.driver.find_element_by_id('your_comments')
+        self.assertTrue('Your comments: Hello! I am some example comments.'
+                        ' I should be in the page after submitting the form'
+                        in commented.text)
+        body = self.driver.find_element_by_xpath('//body')
+        self.assertFalse('I am some other page content' in body.text)
+        self.driver.find_elements_by_link_text('i am a link')[0].click()
+        body = self.driver.find_element_by_xpath('//body')
+        self.assertTrue('I am some other page content' in body.text)
+
+    def tearDown(self):
+        print("Link to your job: https://saucelabs.com/jobs/%s" % self.driver.session_id)
+        self.driver.quit()
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
